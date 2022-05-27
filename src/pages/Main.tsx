ee0b3b63daa5642656e03caf46f08c4729924f79
darry <792478594@qq.com>
2022-05-27 15:31:48 +0800

    '123'

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./Login";
+import AdminIndex from "./AdminIndex";
 
 const Main = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/index" element={<AdminIndex />} />
       </Routes>
     </BrowserRouter>
   );
